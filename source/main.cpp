6e55f8624a3cac714b03ca70825e94b1912823c7
MarcBnl <test@test.nl>
2017-03-21 10:42:00 +0100

    added a quick test for the motors in the main.cpp and updated the UML

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,45 @@
 
 MicroBitDisplay display;
 
+void quickTestMotors(void);
+
 int main() {
-    display.scroll("Robot-trike");
+    display.scroll("3.2.1");
+    quickTestMotors();
 	release_fiber();
 }
+
+void quickTestMotors(void){
+    //define pins
+    MicroBitPin M1P8(MICROBIT_ID_IO_P8,MICROBIT_PIN_P8,PIN_CAPABILITY_BOTH);
+    MicroBitPin M1P12(MICROBIT_ID_IO_P12,MICROBIT_PIN_P12,PIN_CAPABILITY_BOTH);
+    MicroBitPin M2P0(MICROBIT_ID_IO_P0,MICROBIT_PIN_P0,PIN_CAPABILITY_BOTH);
+    MicroBitPin M1P16(MICROBIT_ID_IO_P16,MICROBIT_PIN_P16,PIN_CAPABILITY_BOTH);
+    //coast motors
+    M1P8.setDigitalValue(0);
+    M1P12.setDigitalValue(0);
+    M2P0.setDigitalValue(0);
+    M2P16.setDigitalValue(0);
+    fiber_sleep(1000);
+    //forward motor 1
+    M1P8.setDigitalValue(1);
+    fiber_sleep(5000);
+    M1P8.setDigitalValue(0);
+    //reverse motor 1
+    M1P12.setDigitalValue(1);
+    fiber_sleep(5000);
+    M1P12.setDigitalValue(0);
+    //forward motor 2
+    M2P0.setDigitalValue(1);
+    fiber_sleep(5000);
+    M2P0.setDigitalValue(0);
+    //reverse motor 2
+    M2P16.setDigitalValue(1);
+    fiber_sleep(5000);
+    M2P16.setDigitalValue(0);
+    //coast motors
+    M1P8.setDigitalValue(0);
+    M1P12.setDigitalValue(0);
+    M2P0.setDigitalValue(0);
+    M2P16.setDigitalValue(0);    
+};
