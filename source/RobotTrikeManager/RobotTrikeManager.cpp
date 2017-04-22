a7fed9917322ace2e30a52ac6c284b49bcfd7a54
MarcBnl <marc.berends@gmail.com>
2017-04-22 14:25:01 +0200

    started with the trike manager

--- source/RobotTrikeManager/RobotTrikeManager.cpp
+++ source/RobotTrikeManager/RobotTrikeManager.cpp
@@ -1,1 +1,21 @@
 #include "RobotTrikeManager.h"
+
+
+    int RobotTrikeManager::systemTimerAddComponent(void)
+    {
+        int result=system_timer_add_component(this);// fiber_add_idle_component(this);  //DOES NOT WORK, NO SYSTEM IDLE TICKS RECIEVED
+        if (result==MICROBIT_OK){
+            sendSerial("MICROBIT_OK:fiber_add_idle_component");
+            return MICROBIT_OK;
+        }
+        sendSerial("MICROBIT_NO_RESOURCES:fiber_add_idle_component");
+        return result;
+    };
+
+    void RobotTrikeManager::sendSerial(const char* text)
+    {
+        if (!isDebugOn) return;
+        MicroBit uBit;
+        uBit.serial.printf(text);
+        uBit.serial.printf("\r\n");
+    };
--- source/RobotTrikeManager/RobotTrikeManager.h
+++ source/RobotTrikeManager/RobotTrikeManager.h
@@ -10,13 +10,15 @@
 
     class RobotTrikeManager : public MicroBitComponent
     {
+        static const bool isDebugOn=false;
         public:
-            RobotTrikeManager(void);
-            ~RobotTrikeManager(void);
+            RobotTrikeManager(void){};
+            ~RobotTrikeManager(void){};
             virtual void idleTick(void){};
             virtual void systemTick(void){};
         private:
-
+            int systemTimerAddComponent(void);
+            void sendSerial(const char* text);
     };
 
 #endif /*ROBOTTRIKEMANAGER_H*/
